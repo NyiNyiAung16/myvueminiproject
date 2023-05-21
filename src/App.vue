2f139c583dd1e4b6f457499d890c46b7b19d0499
nyinyi <nyinyi31088@gmail.com>
2023-05-21 21:50:01 +0630

    finsh vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <h1>hi</h1>
-  <Modal></Modal>
+  <h1>Vue cli</h1>
+
+  <div v-if="modalCondition">
+    <Modal :theme="theme" @close="modalCondition=false">
+      <h2>Login Success</h2>
+      <p>Congrat</p>
+      <p>We're looking forward to seeing you</p>
+    </Modal>
+  </div>
+
+   <div v-if="deleteCondition">
+    <modal :themetwo="themetwo" @close="deleteCondition=false">
+      <h2>Are you sure want to delete?</h2>
+      <p>yes or no?</p>
+    </modal>
+   </div>
+
+  <button @click="modalCondition=true">open modal</button>
+  <button @click="deleteCondition=true,change">delete modal</button>
 </template>
+
 
 <script>
 import Modal from "./components/Modal.vue"
 export default {
   components:{
     Modal
-  }
+  },
+  data(){
+    return {
+      theme:"success",
+      themetwo:"delete",
+      modalCondition:false,
+      deleteCondition:false
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+  h1{
+    color: yellowgreen;
+    font-style: italic;
+  }
 </style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-        <p>modal Content</p>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{success:theme==='success',delete:themetwo==='delete'}">
+      <slot>I don't receive any html template</slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name:"ModalView",
+  props:['theme','themetwo'],
+  methods:{
+    closeModal(){
+      this.$emit("close")
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
     .backdrop{
-        background-color: black;
+        top:0;
+        background-color: gray;
         width: 100%;
         height: 100%;
         position: fixed;
     }
     .modal{
         background-color: white;
-        height: 200px;
-        width: 200px;
+        width: 400px;
+        padding: 20px;
+        border-radius: 20px;
+        margin: 100px auto;
+    }
+    .success{
+      background-color: green;
+      color: white;
+    }
+    .delete{
+      background-color: red;
+      color: white;
     }
 </style>
