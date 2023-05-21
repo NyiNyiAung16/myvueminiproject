bb9e85d3a8e08b30715621be28c9824dc1085178
nyinyi <nyinyi31088@gmail.com>
2023-05-21 19:46:36 +0630

    test component

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+        <p>modal Content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .backdrop{
+        background-color: black;
+        width: 100%;
+        height: 100%;
+        position: fixed;
+    }
+    .modal{
+        background-color: white;
+        height: 200px;
+        width: 200px;
+    }
+</style>
